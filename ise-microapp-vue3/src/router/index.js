16881edc1d94f7d87a2fb7843e049f4cfb5550a5
mingjiu <boss_think@163.com>
2025-03-27 11:37:33 +0800

    feat: add Vue3子应用初始化流程

---
+++ ise-microapp-vue3/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Index from '@/views/index'
+import SelectCar from '@/views/select-car'
+
+const routes = [
+  // 首页
+  {
+    path: '/index',
+    name: 'Index',
+    component: Index
+  },
+  // 选车
+  {
+    path: '/select-car',
+    name: 'SelectCar',
+    component: SelectCar
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
