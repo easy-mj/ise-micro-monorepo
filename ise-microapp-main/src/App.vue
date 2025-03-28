7cd8da02ab1e2b792a95f42b6ff55ba0955e5e71
mingjiu <boss_think@163.com>
2025-03-28 17:36:39 +0800

    feat: add 中央控制器-主应用开发

---
+++ ise-microapp-main/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- 头部 -->
+  <main-header></main-header>
+  <!-- 导航 -->
+  <main-nav></main-nav>
+  <div class="sub-container">
+    <!-- loading -->
+    <loading v-if="isLoading"></loading>
+    <!-- 子应用内容 -->
+    <div v-if="!isLoading" class="sub-body">子应用内容</div>
+  </div>
+  <!-- 底部 -->
+  <main-footer></main-footer>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import MainHeader from '@/components/MainHeader'
+import MainNav from '@/components/MainNav'
+import MainFooter from '@/components/MainFooter'
+import Loading from '@/components/Loading'
+
+const isLoading = ref(true)
+setTimeout(() => {
+  isLoading.value = false
+}, 3000)
+</script>
+
+<style lang="scss">
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.sub-container {
+  min-height: 100%;
+  position: relative;
+}
+.sub-body {
+  width: 100%;
+  min-height: 100%;
+}
+</style>
