cb1f8a2a49c021f425200c131cab82c1303ad7d6
mingjiu <boss_think@163.com>
2025-03-28 18:25:17 +0800

    feat: add 菜单路由切换及刷新页面菜单正常回显

--- ise-microapp-main/src/components/MainNav/index.vue
+++ ise-microapp-main/src/components/MainNav/index.vue
@@ -45,69 +45,41 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { NAV_LIST } from '@/constants'
 
-const NAV_LIST = [
-  {
-    name: '首页',
-    status: true,
-    value: 0,
-    url: '/ise-microapp-vue3/#/index',
-    hash: ''
-  },
-  {
-    name: '资讯',
-    status: false,
-    value: 1,
-    url: '/ise-microapp-react15#/information'
-  },
-  {
-    name: '视频',
-    status: false,
-    value: 2,
-    url: '/ise-microapp-react15#/video',
-    hash: ''
-  },
-  {
-    name: '选车',
-    status: false,
-    value: 3,
-    url: '/ise-microapp-vue3/#/select',
-    hash: ''
-  },
-  {
-    name: '新能源',
-    status: false,
-    value: 4,
-    url: '/ise-microapp-vue2#/energy',
-    hash: ''
-  },
-  {
-    name: '新车',
-    status: false,
-    value: 5,
-    url: '/ise-microapp-react16#/new-car',
-    hash: ''
-  },
-  {
-    name: '排行',
-    status: false,
-    value: 6,
-    url: '/ise-microapp-react16#/rank',
-    hash: ''
-  }
-]
+const router = useRouter()
+const route = useRoute()
 
 const currentIndex = ref(0)
 const setCurrentIndex = (item, index) => {
+  if (item.url === route.fullPath) return
   currentIndex.value = index
-  searchStatus.value = item.status
+  // 跳转路由
+  router.push(item.url)
 }
 
 const searchStatus = ref(false)
 const setSearchStatus = (flag) => {
   searchStatus.value = flag
 }
+
+// 监听 route，设置对应的导航状态
+watch(
+  route,
+  (val) => {
+    for (let i = 0; i < NAV_LIST.length; i++) {
+      const url = NAV_LIST[i].url
+      if (val.fullPath.indexOf(url) !== -1) {
+        currentIndex.value = i
+      }
+    }
+  },
+  {
+    deep: true
+  }
+)
 </script>
 
 <style lang="scss" scoped>
---
+++ ise-microapp-main/src/constants/nav.js
@@ -0,0 +1,50 @@
+export const NAV_LIST = [
+  {
+    name: '首页',
+    status: true,
+    value: 0,
+    url: '/ise-microapp-vue3/#/index',
+    hash: ''
+  },
+  {
+    name: '资讯',
+    status: false,
+    value: 1,
+    url: '/ise-microapp-react15#/information'
+  },
+  {
+    name: '视频',
+    status: false,
+    value: 2,
+    url: '/ise-microapp-react15#/video',
+    hash: ''
+  },
+  {
+    name: '选车',
+    status: false,
+    value: 3,
+    url: '/ise-microapp-vue3/#/select',
+    hash: ''
+  },
+  {
+    name: '新能源',
+    status: false,
+    value: 4,
+    url: '/ise-microapp-vue2#/energy',
+    hash: ''
+  },
+  {
+    name: '新车',
+    status: false,
+    value: 5,
+    url: '/ise-microapp-react16#/new-car',
+    hash: ''
+  },
+  {
+    name: '排行',
+    status: false,
+    value: 6,
+    url: '/ise-microapp-react16#/rank',
+    hash: ''
+  }
+]
