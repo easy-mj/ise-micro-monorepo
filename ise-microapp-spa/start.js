1ca708f81e3ed7ad58b9be0f9ccfedbe6c9b193a
mingjiu <boss_think@163.com>
2025-03-28 20:51:39 +0800

    feat: add 微前端框架-路由拦截

--- ise-microapp-spa/start.js
+++ ise-microapp-spa/start.js
@@ -1,4 +1,8 @@
 import { setSubAppList } from './constants'
+import { rewriteRouter } from './router'
+
+// 实现路由拦截
+rewriteRouter()
 
 export const registerMicroApp = (appList) => {
   setSubAppList(appList)
