54e8eba60a63f0a9abc90660e9239e7f210018be
mingjiu <boss_think@163.com>
2025-03-28 10:05:15 +0800

    feat: add vue3子应用接入微前端

--- ise-microapp-vue3/src/main.js
+++ ise-microapp-vue3/src/main.js
@@ -3,4 +3,34 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+let instance = null
+const render = () => {
+  instance = createApp(App)
+  instance.use(store).use(router).mount('#app')
+}
+
+// 判断当前是否是微前端环境中运行
+if (!window.__ISE_MICRO_WEB__) {
+  render()
+}
+
+/**
+ * 暴露三个生命周期方法，供微前端框架使用
+ * 此时，如果在控制台打印输出 window['ise-microapp-vue3'] 是可以查看到这三个生命周期方法
+ */
+
+// 开始加载
+export const bootstrap = () => {
+  console.log('开始加载')
+}
+
+// 渲染成功
+export const mount = () => {
+  render()
+  console.log('渲染成功')
+}
+
+// 卸载
+export const unmount = () => {
+  console.log('卸载', instance)
+}
--- ise-microapp-vue3/vue.config.js
+++ ise-microapp-vue3/vue.config.js
@@ -1,12 +1,42 @@
 const path = require('path')
-const { defineConfig } = require('@vue/cli-service')
+const { name } = require('./package')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 
 const port = 8002
 
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
+  outputDir: 'dist',
+  assetsDir: 'static',
+  filenameHashing: true,
+  publicPath: 'http://localhost:8002',
   devServer: {
     hot: true,
-    port
+    port,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  // 自定义webpack配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    },
+    output: {
+      // 把子应用打包成 umd 库格式，libraryTarget: 'umd' - 将你的 library 暴露为所有的模块定义下都可运行的方式。它将在 CommonJS, AMD 环境下运行，或将模块导出到 global 下的变量。
+      libraryTarget: 'umd',
+      // 打包后的文件名
+      filename: '[name].[hash].js',
+      // 设置后可以在浏览器控制台通过 window['ise-microapp-vue3']获取到当前子应用打包后的所有内容
+      library: 'ise-microapp-vue3',
+      // 使 UMD 模块使用命名的 define
+      umdNamedDefine: true,
+      // 定义全局对象，确保在不同环境正常工作
+      globalObject: 'this'
+    }
   }
-})
+}
